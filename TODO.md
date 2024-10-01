b0c7dbb5b3f2250ebdb67104dedbb574ae85b57f
Nghia <nghia.n.tran@aalto.fi>
2024-10-01 19:00:08 +0300

    clean up index.html, added base.html (jinja), TODO: check TODO.md

---
+++ TODO.md
@@ -0,0 +1,4 @@
+1. clean up other pages
+2. implement a TODO check list
+3. implement crucial functions: add task, delete task, check, uncheck
+4. add JS for API request
--- app.py
+++ app.py
@@ -11,5 +11,17 @@ def index():
 def login():
    return render_template('login.html')
 
+@app.route('/register.html')
+def register():
+   return render_template('register.html')
+
+@app.route('/forgot-password.html')
+def forgot_password():
+   return render_template('forgot-password.html')
+
+@app.route('/404.html')
+def not_found():
+   return render_template('404.html')
+
 if __name__ == '__main__':
   app.run(debug=True)
